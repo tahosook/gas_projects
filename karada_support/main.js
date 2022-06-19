4491115dd1b1909dc2bba498057217cdd3e4442f
Takuya Hosooka <addrblk-reg@yahoo.co.jp>
2022-06-19 11:20:55 +0900

    formatted

--- karada_support/main.js
+++ karada_support/main.js
@@ -12,7 +12,7 @@ function fitbitAuthorize() {
   } else {
     var authorizationUrl = service.getAuthorizationUrl();
     Logger.log('Open the following URL and re-run the script: %s',
-               authorizationUrl);
+      authorizationUrl);
   }
 }
 
@@ -29,29 +29,29 @@ function fitbitReset() {
 function getService() {
   const FITBIT_ID = PropertiesService.getScriptProperties().getProperty("FITBIT_ID");
   const FITBIT_SECRET = PropertiesService.getScriptProperties().getProperty("FITBIT_SECRET");
-  
+
   return OAuth2.createService('FitBit')
-  // Set the endpoint URLs.
-  .setAuthorizationBaseUrl('https://www.fitbit.com/oauth2/authorize')
-  .setTokenUrl('https://api.fitbit.com/oauth2/token')
-  
-  // Set the client ID and secret.
-  .setClientId(CLIENT_ID)
-  .setClientSecret(CLIENT_SECRET)
-  
-  // Set the name of the callback function that should be invoked to
-  // complete the OAuth flow.
-  .setCallbackFunction('fitbitAuthCallback')
-  
-  // Set the property store where authorized tokens should be persisted.
-  .setPropertyStore(PropertiesService.getUserProperties())
-  
-  // Set the scope and additional headers required by the FitBit API.
-  .setScope('weight')
-  .setTokenHeaders({
-    'Authorization': 'Basic ' +
-    Utilities.base64Encode(CLIENT_ID + ':' + CLIENT_SECRET)
-  });
+    // Set the endpoint URLs.
+    .setAuthorizationBaseUrl('https://www.fitbit.com/oauth2/authorize')
+    .setTokenUrl('https://api.fitbit.com/oauth2/token')
+
+    // Set the client ID and secret.
+    .setClientId(CLIENT_ID)
+    .setClientSecret(CLIENT_SECRET)
+
+    // Set the name of the callback function that should be invoked to
+    // complete the OAuth flow.
+    .setCallbackFunction('fitbitAuthCallback')
+
+    // Set the property store where authorized tokens should be persisted.
+    .setPropertyStore(PropertiesService.getUserProperties())
+
+    // Set the scope and additional headers required by the FitBit API.
+    .setScope('weight')
+    .setTokenHeaders({
+      'Authorization': 'Basic ' +
+        Utilities.base64Encode(CLIENT_ID + ':' + CLIENT_SECRET)
+    });
 }
 
 /**
@@ -93,7 +93,7 @@ function postWeightAndFat(weight, bodyfat) {
   });
   result = JSON.parse(response.getContentText());
   Logger.log(JSON.stringify(result, null, 2));
-  
+
   endPointUrl = 'https://api.fitbit.com/1/user/-/body/log/fat.json';
   response = UrlFetchApp.fetch(endPointUrl, {
     method: 'POST',
@@ -140,10 +140,10 @@ function postTweet(status, graph) {
     'method': "POST",
     'payload': { 'media_data': graph }
   };
-  
+
   var mediaUpload = 'https://upload.twitter.com/1.1/media/upload.json';
   var image_upload = JSON.parse(service.fetch(mediaUpload, img_option));
-  
+
   var statusUpdate = 'https://api.twitter.com/1.1/statuses/update.json';
   var response = service.fetch(statusUpdate, {
     method: 'post',
@@ -159,14 +159,14 @@ function postTweet(status, graph) {
 function saveGoogleAndMakeGraph(args) {
   var SHEET_ID = "1IaudaGO3nQFHBj5r6pIcgDZo7g3WW1mpo8B0NoRq9Sk";
   var FOLDER_ID = "1YNF-ThUQ67kYnAd8Pf39_1u4NnVYwmg-";
-  
+
   //可変長引数を取得し、先頭列に日付挿入
   var var_args = [];
   for (var i = 0; i < arguments.length; i++) {
     var_args.push(arguments[i]);
   }
   var_args.unshift(new Date());
-  
+
   // IDからスプレッドシートを取得
   var spreadsheet = SpreadsheetApp.openById(SHEET_ID);
   // 最初のシートを取得
@@ -174,35 +174,35 @@ function saveGoogleAndMakeGraph(args) {
 
   // 最初の行を取得。同じデータなら処理終了
   var prev_values = sheet.getRange(2, 2, 1, 4).getValues();
-  if( +var_args[1] == +prev_values[0][0] &&
-     +var_args[2] == +prev_values[0][1] &&
-     +var_args[3] == +prev_values[0][2] &&
-     +var_args[4] == +prev_values[0][3] ){
+  if (+var_args[1] == +prev_values[0][0] &&
+    +var_args[2] == +prev_values[0][1] &&
+    +var_args[3] == +prev_values[0][2] &&
+    +var_args[4] == +prev_values[0][3]) {
     Logger.log("same as previous data");
 
     return null;
   }
-  
+
   // 1行を追加
   var values = [var_args];
   sheet.insertRowAfter(1);
   sheet.getRange(2, 1, 1, var_args.length).setValues(values);
-  
+
   // chartを取得
-//  var charts = sheet.getCharts();
-//  var chartBlob = charts[0].getBlob();
+  //  var charts = sheet.getCharts();
+  //  var chartBlob = charts[0].getBlob();
 
   var range = sheet.getRange("A1:B15")
   var chart = sheet.getCharts()[0];
   chart = chart.modify().clearRanges()
     .addRange(range)
     .build();
-  sheet.updateChart(chart);  
-  
+  sheet.updateChart(chart);
+
   //フォルダにcreateFileメソッドを実行して、ファイルを作成
   //  var folder = DriveApp.getFolderById(FOLDER_ID);
   //  folder.createFile(chartBlob.getAs('image/png').setName("graph.png"));
-  
+
   return Utilities.base64Encode(chart.getBlob().getBytes());
 }
 
@@ -233,12 +233,12 @@ function main() {
       // Set-Cookieヘッダーからname=valueだけ取り出し、セミコロン以降の属性は除外する
       cookies[i] = cookie[i].split(';')[0];
     };
-    
+
   }
   for (var s in cookies) {
     str += cookies[s] + ";";
   }
-  
+
   // --- json取得
   headers = {
     "Cookie": str
@@ -249,14 +249,14 @@ function main() {
     "muteHttpExceptions": true,
     "headers": headers
   }
-  
+
   var date = new Date();
   var formatDate = Utilities.formatDate(date, "JST", "yyyyMMdd");
   url = "https://healthcare.mb.softbank.jp/v3/web_api_get_home_summary?date=" + formatDate;
   response = UrlFetchApp.fetch(url, options);
   var json = JSON.parse(response);
   // Logger.log("today(" + formatDate + "):" + response);
-  
+
   // 1日前と同じであったら登録しない
   // date.setDate(date.getDate() - 1);
   // formatDate = Utilities.formatDate(date, "JST", "yyyyMMdd");
@@ -271,7 +271,7 @@ function main() {
   // Logger.log("skip");
   // return;
   // }
-  
+
   // --- google spreadsheet & drive(weight graph)
   var b64graph = saveGoogleAndMakeGraph(
     json.root.weight,
@@ -283,14 +283,14 @@ function main() {
     json.root.bone,
     json.root.visceralfat,
     json.root.tbw);
-  
-  if( b64graph != null ) {
+
+  if (b64graph != null) {
     // --- Tweet
     var tweet = "体重=" + json.root.weight + "kg 体脂肪率=" + json.root.bodyfat + "% BMI=" + json.root.bmi;
     Logger.log(tweet);
     postTweet(tweet, b64graph);
-  
-  // --- fitbit
+
+    // --- fitbit
     postWeightAndFat(json.root.weight, json.root.bodyfat);
   }
 }
