1be949c923abd9ae41ed27de3041b753951dff87
Takuya Hosooka <addrblk-reg@yahoo.co.jp>
2025-06-10 21:27:44 +0900

    getYesterdayのテストを更新し、日付フォーマットが"yyyy-MM-dd"であることを確認する正規表現を追加しました。

--- homeoffice_env_visualizer/tests/util.test.ts
+++ homeoffice_env_visualizer/tests/util.test.ts
@@ -4,7 +4,9 @@ describe("Util Tests", () => {
   it("should return the correct value for getYesterday", () => {
     const yesterday = getYesterday();
     expect(yesterday).toBeDefined();
-    const expectedYesterday = "2025-06-09";
-    expect(yesterday).toBe(expectedYesterday);
+
+    // Check if the format is "yyyy-MM-dd"
+    const regex = /^\d{4}-\d{2}-\d{2}$/;
+    expect(yesterday).toMatch(regex);
   });
 });
