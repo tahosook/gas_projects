2545a9eaca9e66a3d61a5753f8d7aaa9e36e472d
Takuya Hosooka <addrblk-reg@yahoo.co.jp>
2025-06-10 20:57:42 +0900

    jest.config.jsにtransform設定を追加し、tsconfig.test.jsonを指定

--- homeoffice_env_visualizer/jest.config.js
+++ homeoffice_env_visualizer/jest.config.js
@@ -1,7 +1,12 @@
+const { transform } = require("typescript");
+
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   globals: {
     Logger: {}
+  },
+  transform: {
+    "^.+\\.ts$": ["ts-jest", {tsconfig: "tsconfig.test.json"}]
   },
 };
