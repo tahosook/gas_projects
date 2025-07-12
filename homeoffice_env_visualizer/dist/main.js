d4f7f67b45f4127c58c6cacfeb9e40014b956b36
Takuya Hosooka <addrblk-reg@yahoo.co.jp>
2025-07-12 11:53:52 +0900

    refactor: 定数を使用してコードの可読性を向上させ、変数宣言をletおよびconstに変更

--- homeoffice_env_visualizer/dist/main.js
+++ homeoffice_env_visualizer/dist/main.js
@@ -18,11 +18,18 @@ Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.getYesterday = getYesterday;
 exports.getAmbidataJson = getAmbidataJson;
 exports.createNewSheet = createNewSheet;
+const HEADER = {
+    DATE: "Date",
+    TEMPERATURE: "Temperature",
+    HUMIDITY: "Humidity",
+    PRESSURE: "Barometric pressure",
+};
+const SUMMARY_SHEET_NAME = "TOTAL";
 // Returns the yesterday date in "yyyy-MM-dd" format.
 function getYesterday() {
-    var yesterday = new Date();
+    const yesterday = new Date();
     yesterday.setDate(yesterday.getDate() - 1);
-    var date = Utilities.formatDate(yesterday, 'Asia/Tokyo', 'yyyy-MM-dd');
+    const date = Utilities.formatDate(yesterday, "Asia/Tokyo", "yyyy-MM-dd");
     return date;
 }
 // Fetches JSON data from ambidata.io for a given channel and date.
@@ -37,69 +44,99 @@ function getAmbidataJson(channelId, readKey, date) {
 // Creates a new sheet in the specified Google Spreadsheet and populates it with data from the provided JSON array.
 function createNewSheet(sheetId, sheetName, json) {
     // IDからスプレッドシートを取得
-    var spreadsheet = SpreadsheetApp.openById(sheetId);
+    const spreadsheet = SpreadsheetApp.openById(sheetId);
     // 最初のシートを取得
     //  var sheet = spreadsheet.getSheets()[0];
     // シートを新規作成
-    var sheet = spreadsheet.insertSheet();
-    var values = [];
-    var MAXROW = 5000000 / 4;
-    var rowcount = json.length > MAXROW ? MAXROW : json.length;
-    for (var i = 0; i < rowcount; i++) {
-        var jst = Utilities.formatDate(new Date(json[i].created), "JST", "yyyy-MM-dd HH:mm:ss");
-        var temp = json[i].d1;
-        var humi = json[i].d2;
-        var bpre = json[i].d3;
+    const sheet = spreadsheet.insertSheet();
+    const values = [];
+    const MAXROW = 5000000 / 4;
+    const rowcount = json.length > MAXROW ? MAXROW : json.length;
+    for (let i = 0; i < rowcount; i++) {
+        const jst = Utilities.formatDate(new Date(json[i].created), "JST", "yyyy-MM-dd HH:mm:ss");
+        const temp = json[i].d1;
+        const humi = json[i].d2;
+        const bpre = json[i].d3;
         values.push([jst, temp, humi, bpre]);
     }
     // シートにデータ登録
-    var STR_C1 = "Date";
-    var STR_C2 = "Temperature";
-    var STR_C3 = "Humidity";
-    var STR_C4 = "Barometric pressure";
-    sheet.appendRow([STR_C1, STR_C2, STR_C3, STR_C4, "", "", STR_C2, STR_C3, STR_C4]);
-    sheet.getRange(sheet.getLastRow() + 1, 1, values.length, values[0].length).setValues(values);
-    var STR_values = [["Average", "=AVERAGE(B2:B290)", "=AVERAGE(C2:C290)", "=AVERAGE(D2:D290)"], ["Min", "=MIN(B2:B290)", "=MIN(C2:C290)", "=MIN(D2:D290)"]];
-    sheet.getRange(2, 6, STR_values.length, STR_values[0].length).setValues(STR_values);
-    var avgValues = [sheet.getRange(2, 7).getValue(), sheet.getRange(2, 8).getValue(), sheet.getRange(2, 9).getValue()];
-    var minValues = [sheet.getRange(3, 7).getValue(), sheet.getRange(3, 8).getValue(), sheet.getRange(3, 9).getValue()];
+    sheet.appendRow([
+        HEADER.DATE,
+        HEADER.TEMPERATURE,
+        HEADER.HUMIDITY,
+        HEADER.PRESSURE,
+        "",
+        "",
+        HEADER.TEMPERATURE,
+        HEADER.HUMIDITY,
+        HEADER.PRESSURE,
+    ]);
+    sheet
+        .getRange(sheet.getLastRow() + 1, 1, values.length, values[0].length)
+        .setValues(values);
+    const STR_values = [
+        [
+            "Average",
+            "=AVERAGE(B2:B290)",
+            "=AVERAGE(C2:C290)",
+            "=AVERAGE(D2:D290)",
+        ],
+        ["Min", "=MIN(B2:B290)", "=MIN(C2:C290)", "=MIN(D2:D290)"],
+    ];
+    sheet
+        .getRange(2, 6, STR_values.length, STR_values[0].length)
+        .setValues(STR_values);
+    const avgValues = [
+        sheet.getRange(2, 7).getValue(),
+        sheet.getRange(2, 8).getValue(),
+        sheet.getRange(2, 9).getValue(),
+    ];
+    const minValues = [
+        sheet.getRange(3, 7).getValue(),
+        sheet.getRange(3, 8).getValue(),
+        sheet.getRange(3, 9).getValue(),
+    ];
     //グラフ作成(気温湿度)
-    var range1 = sheet.getRange("A1:C291");
-    var chart1 = sheet.newChart()
+    const range1 = sheet.getRange("A1:C291");
+    const chart1 = sheet
+        .newChart()
         .addRange(range1)
         .setPosition(5, 5, 0, 0)
         .setChartType(Charts.ChartType.LINE)
-        .setOption('series', {
-        0: { labelInLegend: STR_C2 },
-        1: { labelInLegend: STR_C3, targetAxisIndex: 1 }
+        .setOption("series", {
+        0: { labelInLegend: HEADER.TEMPERATURE },
+        1: { labelInLegend: HEADER.HUMIDITY, targetAxisIndex: 1 },
     })
         .setOption("vAxes", [
-        { title: STR_C2, viewWindow: { min: minValues[0] } },
-        { title: STR_C3, viewWindow: { min: minValues[1] } }
+        { title: HEADER.TEMPERATURE, viewWindow: { min: minValues[0] } },
+        { title: HEADER.HUMIDITY, viewWindow: { min: minValues[1] } },
     ])
         .build();
     sheet.insertChart(chart1);
     //グラフ作成(気圧)
-    var chart2 = sheet.newChart()
+    const chart2 = sheet
+        .newChart()
         .addRange(sheet.getRange("A1:A291"))
         .addRange(sheet.getRange("D1:D291"))
         .setPosition(23, 5, 0, 0)
         .setChartType(Charts.ChartType.LINE)
-        .setOption('series', { 0: { labelInLegend: STR_C4, color: "gold" } })
-        .setOption("vAxes", [
-        { title: STR_C4, viewWindow: { min: minValues[2] } }
-    ])
+        .setOption("series", {
+        0: { labelInLegend: HEADER.PRESSURE, color: "gold" },
+    })
+        .setOption("vAxes", [{ title: HEADER.PRESSURE, viewWindow: { min: minValues[2] } }])
         .build();
     sheet.insertChart(chart2);
-    // シートの名前変更  
+    // シートの名前変更
     sheet.setName(sheetName);
     // 日時のシート作成
-    var sheetTotal = spreadsheet.getSheetByName("TOTAL");
+    let sheetTotal = spreadsheet.getSheetByName(SUMMARY_SHEET_NAME);
     if (!sheetTotal) {
-        sheetTotal = spreadsheet.insertSheet("TOTAL");
+        sheetTotal = spreadsheet.insertSheet(SUMMARY_SHEET_NAME);
     }
     sheetTotal.insertRowAfter(1);
-    sheetTotal.getRange(2, 1, 1, 4).setValues([[sheetName, avgValues[0], avgValues[1], avgValues[2]]]);
+    sheetTotal
+        .getRange(2, 1, 1, 4)
+        .setValues([[sheetName, avgValues[0], avgValues[1], avgValues[2]]]);
 }
 
 
